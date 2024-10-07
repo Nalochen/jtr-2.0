fd8d4fa3117cd5399df78f096f76a233df3bf13a
Nalo Unger <nalo.unger@check24.de>
2024-10-07 21:45:22 +0200

    [2] create first dummy data

---
+++ frontend/apps/desktop/src/app/pages/page-tournament-details/header/tournament-details-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'tournament-details-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tournament-details-header.component.html',
+  styleUrl: './tournament-details-header.component.css',
+})
+export class TournamentDetailsHeaderComponent {}
--- frontend/apps/desktop/src/app/pages/page-tournament-details/tournament-details.component.ts
+++ frontend/apps/desktop/src/app/pages/page-tournament-details/tournament-details.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TournamentDetailsHeaderComponent} from "./header/tournament-details-header.component";
 
 @Component({
   selector: 'app-tournament-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TournamentDetailsHeaderComponent],
   templateUrl: './tournament-details.component.html',
   styleUrl: './tournament-details.component.css',
 })
