0ead3a877545b6e921b9a1782c92ea6001e12089
Nalo Unger <nalo.unger@check24.de>
2024-10-07 20:44:10 +0200

    [2] create new pages

---
+++ frontend/apps/desktop/src/app/pages/page-tournament-details/tournament-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tournament-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tournament-details.component.html',
+  styleUrl: './tournament-details.component.css',
+})
+export class TournamentDetailsComponent {}
---
+++ frontend/apps/desktop/src/app/pages/page-tournament-overview/page-tournament-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tournament-overview',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './page-tournament-overview.component.html',
+  styleUrl: './page-tournament-overview.component.css',
+})
+export class TournamentOverviewComponent {}
