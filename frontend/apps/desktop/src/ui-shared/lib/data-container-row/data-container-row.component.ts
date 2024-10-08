7da9833e5e45bd6eab3c8a4a2b2f8cd630230e1c
Nalo Unger <nalo.unger@check24.de>
2024-10-08 22:21:29 +0200

    [2] setup lib

--- frontend/apps/desktop/src/ui-shared/lib/data-container-row/data-container-row.component.ts
+++ frontend/apps/desktop/src/ui-shared/lib/data-container-row/data-container-row.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 
