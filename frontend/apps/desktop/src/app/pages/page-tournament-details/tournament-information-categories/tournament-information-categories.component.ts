70f5b7bf6a848ed6cfbde0b02c8e7902ef858579
Nalo Unger <nalo.unger@check24.de>
2024-10-10 18:51:23 +0200

    [2] style components

--- frontend/apps/desktop/src/app/pages/page-tournament-details/tournament-information-categories/tournament-information-categories.component.ts
+++ frontend/apps/desktop/src/app/pages/page-tournament-details/tournament-information-categories/tournament-information-categories.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { DataContainerComponent, DataContainerRowComponent } from '../../../ui-shared';
+import { DataContainerComponent, DataContainerRowComponent, ChipComponent, InfoButtonComponent } from '../../../ui-shared';
 import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'tournament-information-categories',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, DataContainerComponent, DataContainerRowComponent, MatIcon],
+  imports: [
+    CommonModule,
+    NgOptimizedImage,
+    DataContainerComponent,
+    DataContainerRowComponent,
+    MatIcon,
+    MatButtonModule,
+    ChipComponent,
+    InfoButtonComponent,
+  ],
   templateUrl: './tournament-information-categories.component.html',
   styleUrl: './tournament-information-categories.component.less',
 })
--- frontend/apps/desktop/src/app/ui-shared/index.ts
+++ frontend/apps/desktop/src/app/ui-shared/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/button/button.component';
 export * from './lib/data-container/data-container.component';
 export * from './lib/data-container-row/data-container-row.component';
+export * from './lib/chip/chip.component';
+export * from './lib/info-button/info-button.component';
