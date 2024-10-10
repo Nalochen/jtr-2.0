021d78da5d8d587e412862f755c86035503510ff
Nalo Unger <nalo.unger@check24.de>
2024-10-10 19:56:56 +0200

    [2] create new components

---
+++ frontend/apps/desktop/src/app/ui-shared/lib/team/team.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'team',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './team.component.html',
+  styleUrl: './team.component.less',
+})
+export class TeamComponent {}
