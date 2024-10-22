6b55eadf61a73cded9e29418db5a925d05259a1d
Nalo Unger <nalo.unger@check24.de>
2024-10-22 09:18:37 +0200

    [16-seite-tournament-details-anlegen] style teams component fix open all button

--- frontend/apps/mobile/src/app/pages/page-tournament-details/teams/tournament-teams.component.ts
+++ frontend/apps/mobile/src/app/pages/page-tournament-details/teams/tournament-teams.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { MatDivider } from '@angular/material/divider';
 import { DataContainerComponent, TeamComponent } from '../../../ui-shared';
@@ -12,26 +12,6 @@ import { MatIcon } from '@angular/material/icon';
   styleUrl: './tournament-teams.component.less',
 })
 export class TournamentTeamsComponent {
-  public registeredTeams: string[] = [
-    'Munich Monks',
-    'Rigor Mortis',
-    'Blue Fangs',
-    'Jugger Helden Bamberg',
-    'Flying Juggmen',
-    'Juggernauts',
-    'Aixcalibur',
-    'Cranium Ex Machina',
-    'Lahnveilchen Gießen',
-    'Torpedo Bääm',
-    'Zonenkinder',
-  ];
-
-  public reservedTeams: string[] = [
-    'NLG',
-    'Peters Pawns',
-    'Quadratur des Kreises',
-    'Quak Pack',
-    'Leere Menge',
-    'Rhein-Neckar-Donner',
-  ];
+  @Input() public registeredTeams: string[] = [];
+  @Input() public reservedTeams: string[] = [];
 }
--- frontend/apps/mobile/src/app/pages/page-tournament-details/tournament-information-categories/tournament-information-categories.component.ts
+++ frontend/apps/mobile/src/app/pages/page-tournament-details/tournament-information-categories/tournament-information-categories.component.ts
@@ -1,9 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { DataContainerComponent, DataContainerRowComponent, ChipComponent, InfoButtonComponent, ButtonComponent } from '../../../ui-shared';
+import {
+  DataContainerComponent,
+  DataContainerRowComponent,
+  ChipComponent,
+  InfoButtonComponent,
+  ButtonComponent,
+  TeamComponent
+} from '../../../ui-shared';
 import { MatIcon } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { TournamentTeamsComponent } from '../teams/tournament-teams.component';
 
 @Component({
   selector: 'tournament-information-categories',
@@ -19,7 +27,9 @@ import { MatDividerModule } from '@angular/material/divider';
     ChipComponent,
     InfoButtonComponent,
     MatDividerModule,
-    ButtonComponent
+    ButtonComponent,
+    TournamentTeamsComponent,
+    TeamComponent
   ],
   templateUrl: './tournament-information-categories.component.html',
   styleUrl: './tournament-information-categories.component.less',
@@ -27,6 +37,7 @@ import { MatDividerModule } from '@angular/material/divider';
 })
 export class TournamentInformationCategoriesComponent {
   protected panels = [
+    { id: 'teams', isOpen: false },
     { id: 'registration', isOpen: false },
     { id: 'contacts', isOpen: false },
     { id: 'address', isOpen: false },
@@ -34,9 +45,36 @@ export class TournamentInformationCategoriesComponent {
     { id: 'other', isOpen: false },
   ];
 
+  public registeredTeams: string[] = [
+    'Munich Monks',
+    'Rigor Mortis',
+    'Blue Fangs',
+    'Jugger Helden Bamberg',
+    'Flying Juggmen',
+    'Juggernauts',
+    'Aixcalibur',
+    'Cranium Ex Machina',
+    'Lahnveilchen Gießen',
+    'Torpedo Bääm',
+    'Zonenkinder',
+  ];
+
+  public reservedTeams: string[] = [
+    'NLG',
+    'Peters Pawns',
+    'Quadratur des Kreises',
+    'Quak Pack',
+    'Leere Menge',
+    'Rhein-Neckar-Donner',
+  ];
+
+  public previewTeams: string[] = this.registeredTeams.slice(0, 6);
+
   get areAllPanelsOpen(): boolean {
     return this.panels.every(panel => panel.isOpen);
   }
+
+  constructor(private cdr: ChangeDetectorRef) {}
 
   protected toggleAllPanels(): void {
     const shouldOpenAll = !this.areAllPanelsOpen;
@@ -44,6 +82,7 @@ export class TournamentInformationCategoriesComponent {
       panel.isOpen = shouldOpenAll;
       console.log(panel.id, panel.isOpen);
     });
+    this.cdr.detectChanges();
   }
 
   protected togglePanel(panelId: string, isOpen: boolean): void {
@@ -51,5 +90,6 @@ export class TournamentInformationCategoriesComponent {
     if (panel && panel.isOpen !== isOpen) {
       panel.isOpen = isOpen;
     }
+    this.cdr.detectChanges();
   }
 }
