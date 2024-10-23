32b6ca2cfa05a0469f482f40859bf65b3ebf4338
Nalo Unger <nalo.unger@check24.de>
2024-10-23 13:40:50 +0200

    [16-seite-tournament-details-anlegen] implement scroll to top button

--- frontend/apps/mobile/src/app/pages/page-tournament-details/bottom-bar/bottom-bar.component.ts
+++ frontend/apps/mobile/src/app/pages/page-tournament-details/bottom-bar/bottom-bar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { ButtonComponent } from '../../../ui-shared';
+import { ScrollToTopComponent } from '../../../ui-shared/lib/scroll-to-top/scroll-to-top.component';
 
 
 @Component({
   selector: 'bottom-bar',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, ButtonComponent],
+  imports: [CommonModule, NgOptimizedImage, ButtonComponent, ScrollToTopComponent],
   templateUrl: './bottom-bar.component.html',
   styleUrl: './bottom-bar.component.less',
 })
