a1c7461361d85ac1ab7414375d18112dedeb1c6e
Nalo Unger <nalo.unger@check24.de>
2024-10-24 01:28:00 +0200

    [16-seite-tournament-details-anlegen] style info button

--- frontend/apps/mobile/src/app/ui-shared/lib/info-button/info-button.component.ts
+++ frontend/apps/mobile/src/app/ui-shared/lib/info-button/info-button.component.ts
@@ -1,23 +1,18 @@
 import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
 import {
-  MAT_DIALOG_DATA,
   MatDialog,
-  MatDialogActions,
-  MatDialogClose, MatDialogContent,
   MatDialogModule,
-  MatDialogRef, MatDialogTitle
 } from '@angular/material/dialog';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import { DialogComponent } from '../dialog/dialog.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @Component({
   selector: 'app-info-button',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatTooltipModule],
+  imports: [CommonModule, MatDialogModule, MatTooltipModule, MatButtonModule],
   templateUrl: './info-button.component.html',
   styleUrl: './info-button.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -28,6 +23,7 @@ export class InfoButtonComponent {
 
   public openTooltipDialog() {
     const dialogRef = this.dialog.open(DialogComponent, {
+      backdropClass: 'dialog-backdrop',
       data: this.infoText,
     });
   }
