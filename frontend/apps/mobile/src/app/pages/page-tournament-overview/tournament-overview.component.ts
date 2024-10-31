6ac3463a5542216c372dfe522d246e10b86ef0da
Nalochen <nala.bibliander@gmail.com>
2024-10-31 14:00:51 +0100

    [15-neue-seite-tournaments-overview-anlegen] page tournament-overview

--- frontend/apps/mobile/src/app/pages/page-tournament-overview/tournament-overview.component.ts
+++ frontend/apps/mobile/src/app/pages/page-tournament-overview/tournament-overview.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DataContainerComponent, DataContainerRowComponent } from '../../ui-shared';
+import { MatButtonModule } from '@angular/material/button';
+import { StatusIndicatorComponent } from '../../ui-shared/lib/status-indicator/status-indicator.component';
 
 @Component({
   selector: 'app-tournament-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DataContainerComponent, MatButtonModule, StatusIndicatorComponent, DataContainerRowComponent],
   templateUrl: './tournament-overview.component.html',
   styleUrl: './tournament-overview.component.less',
 })
-export class TournamentOverviewComponent {}
+export class TournamentOverviewComponent {
+  public teams = ['team1', 'team2', 'team3', 'team4', 'team5', 'team6', 'team7', 'team8', 'team9', 'team10', 'team11', 'team12', 'team13', 'team14', 'team15'];
+}
