f9e421434f3ce7fd378a99f39482d2d4dd48ef42
Nalochen <nala.bibliander@gmail.com>
2024-11-02 18:42:55 +0100

    [82-fe-mo-style-header] style mo header

--- frontend/apps/mobile/src/app/app.component.ts
+++ frontend/apps/mobile/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
   providers: [provideAnimations()],
   imports: [
     RouterModule,
+    MatButtonModule
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
