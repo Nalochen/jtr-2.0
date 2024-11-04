0cffb53425be8d24b619d8d89b1149dffd996585
Nalochen <nala.bibliander@gmail.com>
2024-11-04 16:59:06 +0100

    [4-fe-de-neue-seite-team-details-anlegen] team data

--- frontend/libs/data-domain/store/src/lib/clients/team-data/team-data.client.ts
+++ frontend/libs/data-domain/store/src/lib/clients/team-data/team-data.client.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 import { TeamData } from './team-data.response';
 
-const ENDPOINT = '/api/customer-frontend/get-tournament-details/';
+const ENDPOINT = '/api/customer-frontend/get-team-details/';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +14,16 @@ export class TeamDataClient {
   constructor(private http: HttpClient) {}
 
   public getTeamData$(teamId: number): Observable<TeamData> {
-    return this.http.get<TeamData>(ENDPOINT + teamId.toString());
+    return this.http.get<TeamData>(ENDPOINT + teamId.toString()).pipe(
+      map((response) => ({
+        ...response,
+        createdAt: new Date(response.createdAt),
+        lastTournamentOrganized: new Date(response.lastTournamentOrganized),
+        lastTournamentPlayed: new Date(response.lastTournamentPlayed),
+        founded: new Date(response.founded),
+        trainingTimeUpdatedAt: new Date(response.trainingTimeUpdatedAt),
+        updatedAt: new Date(response.updatedAt),
+      }))
+    );
   }
 }
