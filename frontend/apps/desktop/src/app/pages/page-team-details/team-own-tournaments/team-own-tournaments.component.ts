eef18a2f2ec2e1b2d0aea0d1dd16b442336604a1
LeanderCS <slotosch.leander@outlook.de>
2024-11-07 17:32:14 +0100

    94 | D&M | Mehrere Sprachen hinzufügen

--- frontend/apps/desktop/src/app/pages/page-team-details/team-own-tournaments/team-own-tournaments.component.ts
+++ frontend/apps/desktop/src/app/pages/page-team-details/team-own-tournaments/team-own-tournaments.component.ts
@@ -4,11 +4,12 @@ import {Component, Input} from '@angular/core';
 import { TeamData } from '@jtr/data-domain/store';
 
 import { ButtonComponent } from '../../../ui-shared';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'team-own-tournaments',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, ButtonComponent],
+  imports: [CommonModule, NgOptimizedImage, ButtonComponent, TranslatePipe],
   templateUrl: './team-own-tournaments.component.html',
   styleUrl: './team-own-tournaments.component.less',
 })
