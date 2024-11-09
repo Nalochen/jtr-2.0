8d340f9e8024d6054ffa0055a15ed9075d88d247
Nalochen <nala.bibliander@gmail.com>
2024-11-09 20:26:36 +0100

    [7-neue-seite-register-anlegen] register page

--- frontend/apps/desktop/src/app/pages/page-register/page-register.component.ts
+++ frontend/apps/desktop/src/app/pages/page-register/page-register.component.ts
@@ -1,30 +1,27 @@
 import {CommonModule} from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { registerForm } from '../../business-domain/register/register-form.control';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { ButtonComponent, InfoButtonComponent } from '../../ui-shared';
+import { ButtonComponent, InfoButtonComponent, ButtonColorEnum, ButtonTypeEnum } from '../../ui-shared';
+import { VisibilityButtonComponent } from './visibility-button/visibility-button.component';
 
 
 @Component({
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, InfoButtonComponent, ButtonComponent],
+  imports: [CommonModule, ReactiveFormsModule, MatButtonModule, InfoButtonComponent, ButtonComponent, VisibilityButtonComponent],
   templateUrl: './page-register.component.html',
   styleUrl: './page-register.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageRegisterComponent {
+  protected readonly ButtonColorEnum = ButtonColorEnum;
+  protected readonly ButtonTypeEnum = ButtonTypeEnum;
   protected readonly form = registerForm;
 
-  protected readonly cityInfoText = 'Hier kannst du die Stadt angeben, aus der du kommst oder die in der du trainierst.'
-  protected readonly nameInfoText = 'Dein Anzeige-Name. Trage hier am besten den Namen ein, unter dem dich die meisten Menschen in der Community kennen'
-  protected readonly birthdateInfoText = 'Dein Geburtsdatum wird nur gespeichert, damit du später auf der Turnierseite schneller nach Turnieren filtern kannst, die für deine Altersgruppe freigegeben sind.'
-  protected readonly usernameInfoText = 'Dein Benutzername muss eine eindeutige Abfolge von Zahlen und Buchstaben sein, damit dich jeder eindeutig zuordnen kann.'
-
-  constructor() {}
-
   public onSubmit(): void {
-    if (this.form.valid) {
-      console.log('Form is valid');
+    if (!this.form.valid) {
+      console.log(this.form.controls.password.errors);
     }
   }
 }
--- frontend/apps/desktop/src/app/pages/page-register/visibility-button/visibility-button.component.ts
+++ frontend/apps/desktop/src/app/pages/page-register/visibility-button/visibility-button.component.ts
@@ -1,7 +1,8 @@
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, OnInit } from '@angular/core';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { ButtonComponent, ButtonColorEnum, ButtonTypeEnum } from '../../../ui-shared';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'visibility-button',
@@ -11,16 +12,22 @@ import { ButtonComponent, ButtonColorEnum, ButtonTypeEnum } from '../../../ui-sh
   styleUrl: './visibility-button.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VisibilityButtonComponent {
+export class VisibilityButtonComponent implements OnInit {
+  public readonly control = input.required<FormControl<boolean | null>>();
   public visible = false;
 
   protected readonly ButtonColorEnum = ButtonColorEnum;
   protected readonly ButtonTypeEnum = ButtonTypeEnum;
+
+  public ngOnInit(): void {
+    this.control().setValue(this.visible);
+  }
 
   public toggleVisibility(value: boolean): void {
     if(this.visible === value) {
       return;
     }
     this.visible = value;
+    this.control().setValue(value);
   }
 }
