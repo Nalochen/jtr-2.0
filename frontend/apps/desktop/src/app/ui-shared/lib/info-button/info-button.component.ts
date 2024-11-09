3a28c78410b4de0466793558077ed6faa040b7bc
Nalochen <nala.bibliander@gmail.com>
2024-11-09 15:03:04 +0100

    [7-neue-seite-register-anlegen] style i-button

--- frontend/apps/desktop/src/app/ui-shared/lib/info-button/info-button.component.ts
+++ frontend/apps/desktop/src/app/ui-shared/lib/info-button/info-button.component.ts
@@ -1,31 +1,16 @@
-import { Component, Input, ElementRef, Renderer2, HostListener, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, OverlayPanelModule, MatButtonModule],
   selector: 'app-info-button',
   templateUrl: './info-button.component.html',
   styleUrls: ['./info-button.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
-export class InfoButtonComponent {
-  @Input() public position: 'top' | 'bottom' | 'left' | 'right' = 'top'; // Default position
-  public isTooltipVisible = false;
-
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
-
-  public toggleTooltip(): void {
-    this.isTooltipVisible = !this.isTooltipVisible;
-  }
-
-  @HostListener('document:click', ['$event'])
-  public onClickOutside(event: Event) {
-    if (!this.elementRef.nativeElement.contains(event.target)) {
-      this.isTooltipVisible = false;
-    }
-  }
-}
+export class InfoButtonComponent {}
 
