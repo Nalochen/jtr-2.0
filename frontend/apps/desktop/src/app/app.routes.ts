afa0714e002c09cbc14436b3da2c72f0ea0e6bf6
Nalochen <nala.bibliander@gmail.com>
2024-11-10 16:55:39 +0100

    [5-neue-seite-manage-team-anlegen] update small stuff

--- frontend/apps/desktop/src/app/app.routes.ts
+++ frontend/apps/desktop/src/app/app.routes.ts
@@ -1,17 +1,18 @@
 import { Route } from '@angular/router';
 
-import {TeamDetailsResolver, TeamOverviewResolver} from '@jtr/business-domain/team';
+import { TeamDetailsResolver, TeamOverviewResolver, ManageTeamDetailsResolver} from '@jtr/business-domain/team';
 import { TournamentDetailsResolver, TournamentOverviewResolver } from '@jtr/business-domain/tournament';
-import {UserDetailsResolver} from '@jtr/business-domain/user';
+import { UserDetailsResolver } from '@jtr/business-domain/user';
 
 import { PageOutlineDemoComponent } from './pages/page-outline-demo/page-outline-demo.component';
 import { PageRegisterComponent } from './pages/page-register/page-register.component';
-import {PageTeamDetailsComponent} from './pages/page-team-details/page-team-details.component';
-import {PageTeamOverviewComponent} from './pages/page-team-overview/page-team-overview.component';
+import { PageTeamDetailsComponent } from './pages/page-team-details/page-team-details.component';
+import { PageTeamOverviewComponent } from './pages/page-team-overview/page-team-overview.component';
 import { PageTournamentDetailsComponent } from './pages/page-tournament-details/page-tournament-details.component';
 import { PageTournamentOverviewComponent } from './pages/page-tournament-overview/page-tournament-overview.component';
-import {PageUserDetailsComponent} from './pages/page-user-details/page-user-details.component';
+import { PageUserDetailsComponent } from './pages/page-user-details/page-user-details.component';
 import { SwaggerViewComponent } from './pages/swagger-view/swagger-view.component';
+import { PageManageTeamDetailsComponent } from './pages/page-manage-team/page-manage-team-details.component';
 
 export const appRoutes: Route[] = [
   {
@@ -19,6 +20,13 @@ export const appRoutes: Route[] = [
     component: PageTeamDetailsComponent,
     resolve: {
       teamDetails: TeamDetailsResolver,
+    },
+  },
+  {
+    path: 'manage-team-details/:teamId',
+    component: PageManageTeamDetailsComponent,
+    resolve: {
+      manageTeamDetails: ManageTeamDetailsResolver,
     },
   },
   {
