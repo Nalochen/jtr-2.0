55b519583654c4f043d52f31d8e34114d418f54b
Nalochen <nala.bibliander@gmail.com>
2024-11-17 13:56:15 +0100

    [120-de--mo-turnierdetails-nacharbeiten] showing shoe information

--- frontend/apps/desktop/src/app/pages/page-tournament-details/tournament-information-rules/tournament-information-rules.component.ts
+++ frontend/apps/desktop/src/app/pages/page-tournament-details/tournament-information-rules/tournament-information-rules.component.ts
@@ -8,17 +8,21 @@ import {TournamentData} from '@jtr/data-domain/store';
 import {DataContainerComponent, DataContainerRowComponent, InfoButtonComponent} from '../../../ui-shared';
 import { TranslatePipe } from '@ngx-translate/core';
 import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tournament-information-rules',
   standalone: true,
-  imports: [CommonModule, DataContainerComponent, DataContainerRowComponent, InfoButtonComponent, MatDividerModule, TranslatePipe, InputSwitchModule],
+  imports: [CommonModule, DataContainerComponent, DataContainerRowComponent, InfoButtonComponent, MatDividerModule, TranslatePipe, InputSwitchModule, ReactiveFormsModule],
   templateUrl: './tournament-information-rules.component.html',
   styleUrl: './tournament-information-rules.component.less',
 })
 export class TournamentInformationRulesComponent {
     @Input() public tournament!: TournamentData;
-    protected form = {
-
-    }
+    protected form = new FormGroup({
+      nubbedShoesAllowed: new FormControl(this.tournament.shoes.camAllowed),
+      cleadedShoesAllowed: new FormControl(this.tournament.shoes.cleatsAllowed),
+      studdedShoesAllowed: new FormControl(this.tournament.shoes.studdedAllowed),
+      barefootAllowed: new FormControl(this.tournament.shoes.barefootAllowed),
+    })
 }
