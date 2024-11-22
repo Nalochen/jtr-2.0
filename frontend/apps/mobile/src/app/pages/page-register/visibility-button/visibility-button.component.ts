8e7c561000cee15e1f0a6a27c989001533f978d9
Nalochen <nala.bibliander@gmail.com>
2024-11-22 19:45:12 +0100

    [21-fe-mo-neue-seite-register-anlegen] copy register code from desktop and add route

---
+++ frontend/apps/mobile/src/app/pages/page-register/visibility-button/visibility-button.component.ts
@@ -0,0 +1,34 @@
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { ChangeDetectionStrategy, Component, input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { ButtonColorEnum, ButtonComponent, ButtonTypeEnum } from '../../../ui-shared';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+
+@Component({
+  selector: 'visibility-button',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage, OverlayPanelModule, ButtonComponent],
+  templateUrl: './visibility-button.component.html',
+  styleUrl: './visibility-button.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class VisibilityButtonComponent implements OnInit {
+  public readonly control = input.required<FormControl<boolean | null>>();
+  public visible = false;
+
+  protected readonly ButtonColorEnum = ButtonColorEnum;
+  protected readonly ButtonTypeEnum = ButtonTypeEnum;
+
+  public ngOnInit(): void {
+    this.control().setValue(this.visible);
+  }
+
+  public toggleVisibility(value: boolean): void {
+    if(this.visible === value) {
+      return;
+    }
+    this.visible = value;
+    this.control().setValue(value);
+  }
+}
