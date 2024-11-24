9340bdc424fc030cd05fa52c1a06e2860b945876
Nalochen <nala.bibliander@gmail.com>
2024-11-24 19:23:57 +0100

    [9-fe-de-neue-seite-create-tournament-anlegen] create navigation component

--- frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-navigation/page-create-tournament-navigation.component.ts
+++ frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-navigation/page-create-tournament-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,5 +7,6 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './page-create-tournament-navigation.component.html',
   styleUrl: './page-create-tournament-navigation.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PageCreateTournamentNavigationComponent {}
