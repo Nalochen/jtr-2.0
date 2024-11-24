f3f5c3f2fa43de610c46fc871506edff8ffd9911
Nalochen <nala.bibliander@gmail.com>
2024-11-24 18:17:08 +0100

    [9-fe-de-neue-seite-create-tournament-anlegen] create basic information component

--- frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-information-basic/page-create-tournament-information-basic.component.ts
+++ frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-information-basic/page-create-tournament-information-basic.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DataContainerRowComponent, InfoButtonComponent } from '../../../ui-shared';
+import { InputTextModule } from 'primeng/inputtext';
+import {
+  BasicInformationForm
+} from '../../../../../../../libs/business-domain/tournament/src/lib/form-controls/create-tournament-form.control';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'page-tournament-information-basic',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    DataContainerRowComponent,
+    InputTextModule,
+    ReactiveFormsModule,
+    InfoButtonComponent
+  ],
   templateUrl: './page-create-tournament-information-basic.component.html',
   styleUrl: './page-create-tournament-information-basic.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PageTournamentInformationBasicComponent {}
+export class PageTournamentInformationBasicComponent {
+  @Input() form!: FormGroup<BasicInformationForm>;
+}
