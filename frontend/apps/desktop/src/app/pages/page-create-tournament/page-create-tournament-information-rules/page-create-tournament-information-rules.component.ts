79a9e5ef8050e5091e47b684fcb4c5f858524a1f
Nalochen <nala.bibliander@gmail.com>
2024-11-25 14:05:24 +0100

    [9-fe-de-neue-seite-create-tournament-anlegen] create shoes component

--- frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-information-rules/page-create-tournament-information-rules.component.ts
+++ frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-information-rules/page-create-tournament-information-rules.component.ts
@@ -1,14 +1,26 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import {
   RulesInformationForm
 } from '../../../../../../../libs/business-domain/tournament/src/lib/form-controls/create-tournament-form.control';
+import { DataContainerRowComponent, InfoButtonComponent } from '../../../ui-shared';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputSwitchModule } from 'primeng/inputswitch';
 
 @Component({
   selector: 'page-create-tournament-information-rules',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    DataContainerRowComponent,
+    InputTextModule,
+    InputTextareaModule,
+    InputSwitchModule,
+    InfoButtonComponent
+  ],
   templateUrl: './page-create-tournament-information-rules.component.html',
   styleUrl: './page-create-tournament-information-rules.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
