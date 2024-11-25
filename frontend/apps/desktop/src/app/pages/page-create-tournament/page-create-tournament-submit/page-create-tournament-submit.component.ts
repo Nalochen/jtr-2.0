826dd80e6f01abf982151aed1d44ebe4ce6fc2ee
Nalochen <nala.bibliander@gmail.com>
2024-11-25 14:27:56 +0100

    [9-fe-de-neue-seite-create-tournament-anlegen] create submit section component

--- frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-submit/page-create-tournament-submit.component.ts
+++ frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-submit/page-create-tournament-submit.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent, ButtonTypeEnum, ButtonColorEnum, ButtonFunctionType } from '../../../ui-shared';
 
 @Component({
   selector: 'page-create-tournament-submit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ButtonComponent
+  ],
   templateUrl: './page-create-tournament-submit.component.html',
   styleUrl: './page-create-tournament-submit.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PageCreateTournamentSubmitComponent {}
+export class PageCreateTournamentSubmitComponent {
+  public readonly ButtonColorEnum = ButtonColorEnum;
+  public readonly ButtonTypeEnum = ButtonTypeEnum;
+  public readonly ButtonFunctionType = ButtonFunctionType;
+
+  public onManageTeams() {
+    console.log('Manage Teams');
+  }
+
+  public onPublish() {
+    console.log('Publish');
+  }
+}
