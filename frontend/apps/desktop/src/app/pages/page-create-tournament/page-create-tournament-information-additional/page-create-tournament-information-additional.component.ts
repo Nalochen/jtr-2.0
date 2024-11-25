03d3f151e0ef45a822f1c6f323096e9621c5fcd5
Nalochen <nala.bibliander@gmail.com>
2024-11-25 14:08:41 +0100

    [9-fe-de-neue-seite-create-tournament-anlegen] create additional component

--- frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-information-additional/page-create-tournament-information-additional.component.ts
+++ frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-information-additional/page-create-tournament-information-additional.component.ts
@@ -1,14 +1,21 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import {
   RegistrationInformationForm
 } from '../../../../../../../libs/business-domain/tournament/src/lib/form-controls/create-tournament-form.control';
+import { DataContainerRowComponent } from '../../../ui-shared';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @Component({
   selector: 'page-create-tournament-information-additional',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    DataContainerRowComponent,
+    InputTextareaModule
+  ],
   templateUrl: './page-create-tournament-information-additional.component.html',
   styleUrl: './page-create-tournament-information-additional.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
