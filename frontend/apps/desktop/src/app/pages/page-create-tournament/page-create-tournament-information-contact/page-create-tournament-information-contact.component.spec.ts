ec55e8920785fbd5af71f6689ee42d08a602a90a
Nalochen <nala.bibliander@gmail.com>
2024-11-25 18:01:58 +0100

    [9-fe-de-neue-seite-create-tournament-anlegen] fix tests

--- frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-information-contact/page-create-tournament-information-contact.component.spec.ts
+++ frontend/apps/desktop/src/app/pages/page-create-tournament/page-create-tournament-information-contact/page-create-tournament-information-contact.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PageCreateInformationContactComponent } from './page-create-tournament-information-contact.component';
+import { PageCreateTournamentInformationContactComponent } from './page-create-tournament-information-contact.component';
 
-describe('PageCreateInformationContactComponent', () => {
-  let component: PageCreateInformationContactComponent;
-  let fixture: ComponentFixture<PageCreateInformationContactComponent>;
+describe('PageCreateTournamentInformationContactComponent', () => {
+  let component: PageCreateTournamentInformationContactComponent;
+  let fixture: ComponentFixture<PageCreateTournamentInformationContactComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageCreateInformationContactComponent],
+      imports: [PageCreateTournamentInformationContactComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(PageCreateInformationContactComponent);
+    fixture = TestBed.createComponent(PageCreateTournamentInformationContactComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
