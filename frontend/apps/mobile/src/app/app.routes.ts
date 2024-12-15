2b1dbd270fcda114b9e1d596c8a8d7b11ac87f34
Nalochen <nala.bibliander@gmail.com>
2024-12-15 16:12:01 +0100

    [17-neue-seite-teams-overview-anlegen] created new mobile page

--- frontend/apps/mobile/src/app/app.routes.ts
+++ frontend/apps/mobile/src/app/app.routes.ts
@@ -41,7 +41,7 @@ export const appRoutes: Route[] = [
     },
   },
   {
-    path: 'team-overview',
+    path: 'teams-overview',
     component: PageTeamOverviewComponent,
     resolve: {
       teamDetails: TeamOverviewResolver,
---
+++ frontend/apps/mobile/src/app/pages/page-team-overview/team-arrow/team-arrow.component.ts
@@ -0,0 +1,18 @@
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {Component, Input} from '@angular/core';
+
+import {MatIconButton} from '@angular/material/button';
+
+import { ButtonComponent, DataContainerComponent } from '../../../ui-shared';
+import { StatusIndicatorComponent } from '../../../ui-shared';
+
+@Component({
+  selector: 'arrow',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage, DataContainerComponent, MatIconButton, StatusIndicatorComponent, ButtonComponent],
+  templateUrl: './team-arrow.component.html',
+  styleUrl: './team-arrow.component.less',
+})
+export class TeamArrowComponent {
+  @Input() public hasPlacementChangedUpwards!: boolean | null;
+}
