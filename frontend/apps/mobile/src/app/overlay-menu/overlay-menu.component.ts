9806c164d41b82369841d4fc3a82aa48b0f43def
Nalochen <nala.bibliander@gmail.com>
2024-12-31 12:52:05 +0100

    [154-de-mo-manage-team-nacharbeiten] add routing for menu

--- frontend/apps/mobile/src/app/overlay-menu/overlay-menu.component.ts
+++ frontend/apps/mobile/src/app/overlay-menu/overlay-menu.component.ts
@@ -3,6 +3,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 
 import { ButtonColorEnum, ButtonComponent, ButtonTypeEnum } from '../ui-shared';
 import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { SingletonGetter } from '@jtr/infrastructure/cache';
+import { Observable } from 'rxjs';
+import { AuthService } from '../business-rules/auth/auth.service';
 
 @Component({
   selector: 'app-overlay-menu',
@@ -19,7 +23,11 @@ export class OverlayMenuComponent {
 
   @Output() public menuClose = new EventEmitter<void>();
 
-  constructor(private translate: TranslateService) {
+  constructor(
+    private translate: TranslateService,
+    private router: Router,
+    private authService: AuthService
+  ) {
     const savedLanguage = sessionStorage.getItem('language') || 'de';
     this.translate.setDefaultLang(savedLanguage);
     this.translate.use(savedLanguage);
@@ -32,6 +40,53 @@ export class OverlayMenuComponent {
   public closeMenu() {
     this.isOpen = false;
     this.menuClose.emit();
+  }
+
+  public onRedirectToTournaments() {
+    this.router.navigate(['/tournament-overview']);
+    this.closeMenu()
+  }
+
+  public onRedirectToTeams() {
+    this.router.navigate(['/teams-overview']);
+    this.closeMenu()
+  }
+
+
+  public onRedirectToAccount() {
+    this.router.navigate(['/manage-user-details']);
+    this.closeMenu()
+  }
+
+  public onRedirectToLinks() {
+    //this.router.navigate(['']);
+    this.closeMenu()
+  }
+
+  public onRedirectToLogin() {
+    this.router.navigate(['/login']);
+    this.closeMenu()
+  }
+
+  public onRedirectToAboutUs() {
+    //this.router.navigate(['']);
+    this.closeMenu()
+  }
+
+  public onRedirectToImpressum() {
+    //this.router.navigate(['']);
+    this.closeMenu()
+  }
+
+  @SingletonGetter()
+  public get isLoggedIn$(): Observable<boolean> {
+    return this.authService.isAuthenticated$;
+  }
+
+  public logout(): void {
+    this.authService.logout();
+
+    this.router.navigate(['/']);
   }
 
   public switchLanguage(language: string) {
