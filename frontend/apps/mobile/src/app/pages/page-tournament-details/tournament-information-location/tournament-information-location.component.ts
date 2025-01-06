bdeffc334ea189e8e527efd9a32904bae2882d8f
Nalochen <nala.bibliander@gmail.com>
2025-01-06 23:49:24 +0100

    [220-de-mo-turnierdetails-nacharbeiten] translate food texts; only show urls when existing

--- frontend/apps/mobile/src/app/pages/page-tournament-details/tournament-information-location/tournament-information-location.component.ts
+++ frontend/apps/mobile/src/app/pages/page-tournament-details/tournament-information-location/tournament-information-location.component.ts
@@ -44,4 +44,48 @@ export class TournamentInformationLocationComponent extends DataContainerExpanda
         return 'page-tournament-details.accommodation-type-other';
     }
   }
+
+  public get foodEveningText(): string {
+    switch (this.tournament.food?.evening) {
+      case 'provided':
+        return 'page-tournament-details.food-evening-provided';
+      case 'grill_available':
+        return 'page-tournament-details.food-evening-grill-available';
+      default:
+        return 'page-tournament-details.food-evening-no';
+    }
+  }
+
+  public get foodGastroText(): string {
+    switch (this.tournament.food?.gastro) {
+      case 'on_the_course':
+        return 'page-tournament-details.food-gastro-on-the-course';
+      case 'near':
+        return 'page-tournament-details.food-gastro-near';
+      case 'far':
+        return 'page-tournament-details.food-gastro-far';
+      default:
+        return 'page-tournament-details.food-gastro-no';
+    }
+  }
+
+  public get foodMorningText(): string {
+    switch (this.tournament.food?.morning) {
+      case 'provided':
+        return 'page-tournament-details.food-morning-provided';
+      default:
+        return 'page-tournament-details.food-morning-no';
+    }
+  }
+
+  public get foodNoonText(): string {
+    switch (this.tournament.food?.noon) {
+      case 'provided':
+        return 'page-tournament-details.food-noon-provided';
+      case 'snacks':
+        return 'page-tournament-details.food-noon-snacks';
+      default:
+        return 'page-tournament-details.food-noon-no';
+    }
+  }
 }
